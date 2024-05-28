eea7a67a931144d1c384a2e5c6ea610f8cc50886
Guillaume Labussiere <labussieremailpro@gmail.com>
2024-05-28 20:23:30 +0200

    suite

--- code/test.php
+++ code/test.php
@@ -36,26 +36,25 @@ echo($salade->getColor());
 
 
 
-//class Spinach extends Vegetable {
-//    public $cooked = false;
-//
-//    public function __construct()
-//    {
-//        parent::__construct(true, "green");
-//    }
-//
-//    public function cook()
-//    {
-//        $this->cooked = true;
-//    }
-//
-//    public function isCooked()
-//    {
-//        return $this->cooked;
-//    }
-//}
-//$Spinach = new Spinach();
-//$Spinach->cook();
-//echo($Spinach->isCooked() ? "oui" : "non" );
+class Spinach extends Vegetable {
+    public $cooked = false;
 
+    public function __construct()
+    {
+        parent::__construct(true, "green");
+    }
+
+    public function cook()
+    {
+        $this->cooked = true;
+    }
+
+    public function isCooked()
+    {
+        return $this->cooked;
+    }
+}
+$Spinach = new Spinach();
+$Spinach->cook();
+echo($Spinach->isCooked() ? "oui" : "non" );
 ?>
